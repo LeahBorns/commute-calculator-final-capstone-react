d253c866507e67d59f745fabfa8880c3b74bb071
Leah Borns <leah.borns@gmail.com>
2018-02-01 17:53:42 -0600

    added demo account info

--- js/components/signin.js
+++ js/components/signin.js
@@ -21,6 +21,7 @@ export default class Signin extends React.Component {
         <section id="sign-in-section">
         <header id="sign-in-header">
         <h3>Sign-in to Co<sub>2</sub>mmute Calculator</h3>
+        <p>For demo account use: un: admin pw: admin</p>
         </header>
         <form id="signin-form" >
         <div>
